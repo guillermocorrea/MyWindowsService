2f4c580a58bbfa8e01c6c1a925a987d0cf03c45e
Guillermo Correa <guille_freelance@hotmail.com>
2012-08-20 16:06:24 -0500

    initial commit

---
+++ ServicioNotificacionAportes/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServicioNotificacionAportes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("ServicioNotificacionAportes")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c82a1619-9c32-4412-981d-873d65a8b74c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
---
+++ ServicioNotificacionAportes/Service1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+using System.IO;
+
+namespace ServicioNotificacionAportes
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer t = null;
+
+        public Service1()
+        {
+            InitializeComponent();
+            // System.Configuration.ConfigurationManager.AppSettings["Intervalo"];
+            t = new Timer(Convert.ToDouble(System.Configuration.ConfigurationManager.AppSettings["Intervalo"]));
+            t.Elapsed += new ElapsedEventHandler(t_Elapsed);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            t.Start(); //Iniciamos el timer
+        }
+
+        protected override void OnStop()
+        {
+            t.Stop();
+        }
+
+        void t_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                string path = @"C:\log.txt";
+                TextWriter tw = new StreamWriter(path, true);
+                tw.WriteLine("A fecha de : " + DateTime.Now.ToString() + ", Intervalo: " + t.Interval.ToString());
+                tw.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.EventLog.WriteEntry("Application", "Exception: " + ex.Message);
+            }
+        }
+    }
+}
